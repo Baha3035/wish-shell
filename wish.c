db0a5ac7f5c1ff17ba08e9a603ac74498a42d3df
Baha3035 <batmaaanpls@gmail.com>
2024-02-01 17:48:53 +0600

    feature: add built-in commands

--- wish.c
+++ wish.c
@@ -4,9 +4,13 @@
 #include <unistd.h>
 
 #define BUFFER_SIZE 4096
+#define MAX_PATHS 10
+#define MAX_PATH_LENGTH 256
+#define MAX_ARGS 32
 
 int main(int argc, char* argv[]){
-    char PATH[] = "/bin/";
+    char error_message[] = "An error has occurred\n";
+    char *PATH[MAX_PATHS];
     // strcat(PATH, "/");
     char buffer[BUFFER_SIZE];
 
@@ -14,23 +18,67 @@ int main(int argc, char* argv[]){
 
     while (fgets(buffer, BUFFER_SIZE, stdin) != NULL)
     {
+        if (strcmp(buffer, "\n") == 0){
+            printf("wish> ");
+            continue;
+        }
         // Remove newline character from the end of the input
         buffer[strcspn(buffer, "\n")] = '\0';
 
-        printf("buffer %s\n", buffer);
+        // printf("buffer %s\n", buffer);
 
         char* token;
-        char* args[32];
-        int i = 0;
+        char* args[MAX_ARGS] = {NULL}; // Initialize args with NULL pointers
+        int arg_count = 0;
         // strtok() returns a pointer
         token = strtok(buffer, " ");
-        while (token != NULL && i < 32) {
-            printf("token %s\n", token);
-            args[i++] = token;
+        while (token != NULL && arg_count < MAX_ARGS - 1) {
+            // printf("token %s\n", token);
+            args[arg_count++] = token;
             token = strtok(NULL, " ");
         }
 
-        args[i] = NULL; // Null-terminate the arguments list
+        if (strcmp(args[0], "exit") == 0) {
+            exit(0);
+        } else if (strcmp(args[0], "cd") == 0) {
+            int cnt = 0;
+            int j = 1;
+
+            while (j < MAX_ARGS && args[j] != NULL)
+            {
+                cnt++;
+            }
+
+            if (cnt == 1){
+                if (chdir(args[0]) != 0){
+                    write(STDERR_FILENO, error_message, strlen(error_message));
+                    printf("wish> ");
+                    continue;
+                }
+            } else {
+                write(STDERR_FILENO, error_message, strlen(error_message));
+                printf("wish> ");
+                continue;
+            }
+        } else if (strcmp(args[0], "path") == 0 && args[1] == NULL) {
+            printf("wish> ");
+            continue;
+            // for (int j = 1; j < 32; j++) {
+            //     if (args[j] != NULL) {
+            //         PATH[j-1] = args[j];
+            //     }
+            //     else {
+            //         break;
+            //     }
+            // }
+        } else if (strcmp(args[0], "path") == 0) {
+            int j = 1;
+
+            while (j < 32 && args[j] != NULL)
+            {
+                PATH[j-1] = args[j];
+            }
+        }
 
         pid_t pid = fork();
 
@@ -40,24 +88,23 @@ int main(int argc, char* argv[]){
             exit(EXIT_FAILURE);
         } else if (pid == 0){
             // Child process
-            char bin_command_path[BUFFER_SIZE];
-            char user_bin_command_path[BUFFER_SIZE];
-            snprintf(bin_command_path, BUFFER_SIZE, "%s%s", PATH, args[0]);
-            // printf("Bin command path %s\n",bin_command_path);
-            snprintf(user_bin_command_path, BUFFER_SIZE, "%s%s", PATH, args[0]);
-            if (access(bin_command_path, X_OK) == 0){
-                execv(bin_command_path, args);
-                // If execv returns it must've failed
-                perror("execv");
-                exit(EXIT_FAILURE);
-            } else if (access(user_bin_command_path, X_OK) == 0){
-                execv(user_bin_command_path, args);
-                // If execv returns it must've failed
-                perror("execv");
-                exit(EXIT_FAILURE);
-            } else {
-                perror("command not found");
-                exit(EXIT_FAILURE);
+            int path_success = 0;
+            for (int j = 0; j < MAX_PATHS; j++) {
+                if (path_success == 1){
+                    break;
+                }
+                char command_path[MAX_PATH_LENGTH];
+                snprintf(command_path, MAX_PATH_LENGTH, "%s%s", PATH[j], args[0]);
+                if (access(command_path, X_OK) == 0){
+                    path_success = 1;
+                    execv(command_path, args);
+                    // If execv returns it must've failed
+                    perror("execv");
+                    exit(EXIT_FAILURE);
+                } else {
+                    perror("command not found");
+                    exit(EXIT_FAILURE);
+                }
             }
         } else {
             // Parent process
