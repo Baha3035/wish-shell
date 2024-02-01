2c748b497e136468314732b080cc33cb68d969aa
Baha3035 <batmaaanpls@gmail.com>
2024-02-01 18:19:59 +0600

    feature: dynamic path addition

--- wish.c
+++ wish.c
@@ -41,81 +41,61 @@ int main(int argc, char* argv[]){
         if (strcmp(args[0], "exit") == 0) {
             exit(0);
         } else if (strcmp(args[0], "cd") == 0) {
-            int cnt = 0;
-            int j = 1;
-
-            while (j < MAX_ARGS && args[j] != NULL)
-            {
-                cnt++;
-            }
-
-            if (cnt == 1){
-                if (chdir(args[0]) != 0){
-                    write(STDERR_FILENO, error_message, strlen(error_message));
-                    printf("wish> ");
-                    continue;
-                }
-            } else {
+            // Change directory
+            if (arg_count == 2 && chdir(args[1]) != 0) {
                 write(STDERR_FILENO, error_message, strlen(error_message));
+            }
+        } else if (strcmp(args[0], "path") == 0) {
+            if (arg_count == 1) {
                 printf("wish> ");
                 continue;
-            }
-        } else if (strcmp(args[0], "path") == 0 && args[1] == NULL) {
-            printf("wish> ");
-            continue;
-            // for (int j = 1; j < 32; j++) {
-            //     if (args[j] != NULL) {
-            //         PATH[j-1] = args[j];
-            //     }
-            //     else {
-            //         break;
-            //     }
-            // }
-        } else if (strcmp(args[0], "path") == 0) {
-            int j = 1;
-
-            while (j < 32 && args[j] != NULL)
-            {
-                PATH[j-1] = args[j];
-            }
-        }
-
-        pid_t pid = fork();
-
-        if (pid < 0){
-            // Fork failed
-            perror("fork");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0){
-            // Child process
-            int path_success = 0;
-            for (int j = 0; j < MAX_PATHS; j++) {
-                if (path_success == 1){
-                    break;
+            } else {
+                // Clear existing PATH
+                for (int i = 0; i < MAX_PATHS; i++) {
+                    PATH[i] = NULL;
                 }
-                char command_path[MAX_PATH_LENGTH];
-                snprintf(command_path, MAX_PATH_LENGTH, "%s%s", PATH[j], args[0]);
-                if (access(command_path, X_OK) == 0){
-                    path_success = 1;
-                    execv(command_path, args);
-                    // If execv returns it must've failed
-                    perror("execv");
-                    exit(EXIT_FAILURE);
-                } else {
-                    perror("command not found");
-                    exit(EXIT_FAILURE);
+                // Set new PATH
+                for (int j = 1; j < arg_count && j < MAX_PATHS; j++) {
+                    // Append '/' to the end of the directory path
+                    size_t len = strlen(args[j]);
+                    if (len > 0 && args[j][len-1] != '/'){
+                        strcat(args[j], "/");
+                    }
+                    PATH[j - 1] = args[j];
                 }
             }
         } else {
-            // Parent process
-            int status;
-            waitpid(pid, &status, 0);
-            printf("wish> ");
-        }
+            pid_t pid = fork();
 
-        // char* arr[] = {buffer, "-l", NULL};
-        // execv(PATH, arr);
-        /* code */
+            if (pid < 0){
+                // Fork failed
+                perror("fork");
+                exit(EXIT_FAILURE);
+            } else if (pid == 0){
+                // Child process
+                int path_success = 0;
+                for (int j = 0; j < MAX_PATHS; j++) {
+                    char command_path[MAX_PATH_LENGTH];
+                    snprintf(command_path, MAX_PATH_LENGTH, "%s%s", PATH[j], args[0]);
+                    if (access(command_path, X_OK) == 0){
+                        path_success = 1;
+                        execv(command_path, args);
+                        // If execv returns it must've failed
+                        perror("execv");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                if (!path_success) {
+                    perror("command not found");
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                // Parent process
+                int status;
+                waitpid(pid, &status, 0);
+            }
+        }
+        printf("wish> ");
     }
     
     return 0;
